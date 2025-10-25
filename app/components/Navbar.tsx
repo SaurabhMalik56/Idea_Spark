88e5d0cd181df2b741794271382f89889cb6ed1f
Saurabh Malik <saurabhmalikk56@gmail.com>
2025-10-25 06:37:58 +0530

    Added Authentication From Scratch

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { auth, signIn, signOut } from "@/auth";
+
+async function Navbar() {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button
+                  type="submit"
+                  className="bg-transparent border border-red-500 text-red-500 px-4 py-2 rounded cursor-pointer hover:bg-red-500 hover:text-white transition"
+                >
+                  Logout
+                </button>
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button
+                type="submit"
+                className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-600 transition"
+              >
+                Login
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
